0131b61f194284095e796d0dc95cd2acb0311a57
yushin <am1213@naver.com>
2023-06-26 21:22:40 +0900

    # list, detail test

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from database import get_db
-from domain.question import question_schema
-from models import Question
+from domain.question import question_schema, question_service
 
 router = APIRouter(
     prefix="/api/question"
@@ -12,5 +11,11 @@ router = APIRouter(
 
 @router.get("/list", response_model=list[question_schema.Question])
 def question_list(db: Session = Depends(get_db)):
-    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    _question_list = question_service.get_question_list(db)
     return _question_list
+
+
+@router.get("/detail/{question_id}", response_model=question_schema.Question)
+def question_detail(question_id: int, db: Session = Depends(get_db)):
+    question = question_service.get_question(db, question_id=question_id)
+    return question
